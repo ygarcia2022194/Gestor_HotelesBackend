47198cb2aa4ddfb1b076e271abcbd8437c803cab
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-05-20 10:04:56 -0600

    Se hicieron cambios en service

---
+++ src/services/services.routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+    createService,
+    getServices,
+    getServiceById,
+    updateService,
+    deleteService
+} from '../controllers/serviceController.js';
+
+const router = express.Router();
+
+router.post('/', createService);
+router.get('/', getServices);
+router.get('/:id', getServiceById);
+router.put('/:id', updateService);
+router.delete('/:id', deleteService);
+
+export default router;
